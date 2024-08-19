ee6b527f1d476a192b151dfb4519b9ca3257db59
GWRT <gramosti@unsa.edu.pe>
2024-08-19 18:43:21 -0500

    EstudianteNew Changed

--- src/views/Estudiantes/EstudianteNew.vue
+++ src/views/Estudiantes/EstudianteNew.vue
@@ -1,16 +1,3 @@
-<script setup>
-const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-        console.log(file.name);
-    }
-};
-
-const triggerFileInput = () => {
-    document.getElementById('profile_image').click();
-};
-</script>
-
 <template>
     <!-- component -->
     <div class="flex items-center justify-center w-full p-6 bg-quinary text-neutral">
@@ -24,7 +11,7 @@ const triggerFileInput = () => {
                     </div>
 
                     <div class="lg:col-span-2">
-                        <form v-on:submit="saveStudentsForm">
+                        <form @submit="saveStudentsForm">
                             <div class="grid grid-cols-1 gap-3 text-sm gap-y-1 md:grid-cols-2">
 
                                 <div class="md:col-span-1">
@@ -41,10 +28,9 @@ const triggerFileInput = () => {
                                 <div class="flex flex-col items-center overflow-hidden md:col-span-1 md:row-span-10">
                                     <label for="profile_image" class="mt-3 font-semibold md:mt-0">Foto de perfil</label>
                                     <input type="file" accept="image/png, image/jpg, image/jpeg" name="profile_image"
-                                        id="profile_image" class="hidden" @change="handleFileChange"
-                                        v-on:change="previewImage" />
+                                        id="profile_image" class="hidden" @change="previewImage" />
 
-                                    <img v-if="this.foto" :src="this.foto" id="fotoimg" alt="Profile Picture"
+                                    <img v-if="foto" :src="foto" id="fotoimg" alt="Profile Picture"
                                         class="w-32 h-32 my-2 rounded-full">
                                     <img v-else
                                         src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-256.png"
@@ -74,50 +60,52 @@ const triggerFileInput = () => {
     </div>
 </template>
 
-<script>
+<script setup>
+import ref from 'vue'
 import { showAlert, sendResultResponse } from '@/components/Utils/funciones.js'
 
-export default {
-    data() {
-        return {
-            nombre: '',
-            apellido: '',
-            foto: '',
-            url: `${import.meta.env.VITE_ACADEMICO_API_URL}/api/v1/estudiantes`,
-            loading: false
+const nombre = ref('');
+const apellido = ref('');
+const foto = ref('');
+const url = `${import.meta.env.VITE_ACADEMICO_API_URL}/api/v1/estudiantes`;
+const loading = ref(false);
+
+const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        console.log(file.name);
+    }
+};
+
+const triggerFileInput = () => {
+    document.getElementById('profile_image').click();
+};
+
+const previewImage = (event) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = () => {
+        foto.value = reader.result;
+    }
+};
+
+const saveStudentsForm = (event) => {
+    this.loading = true
+    event.preventDefault();
+
+    if (nombre.value.trim() === '') {
+        showAlert('Ingrese un nombre', 'warning', 'first_name');
+    } else if (apellido.value.trim() === '') {
+        showAlert('Ingrese un apellido', 'warning', 'last_name');
+    } else {
+        const params = {
+            nombre: nombre.value.trim(),
+            apellido: apellido.value.trim(),
+            foto: foto.value.trim()
         }
-    },
-    methods: {
-        saveStudentsForm() {
-            this.loading = true
-            event.preventDefault();
-            var myImage = document.getElementById('fotoimg');
-            this.foto = myImage.src;
 
-            if (this.nombre.trim() === '') {
-                showAlert('Ingrese un nombre', 'warning', 'first_name');
-            } else if (this.apellido.trim() === '') {
-                showAlert('Ingrese un apellido', 'warning', 'last_name');
-            } else {
-                var params = {
-                    nombre: this.nombre.trim(),
-                    apellido: this.apellido.trim(),
-                    foto: this.foto.trim()
-                }
-
-                sendResultResponse('POST', params, this.url, 'Estudiante registrado correctamente');
-            }
-        },
-        previewImage(event) {
-            var reader = new FileReader();
-            reader.readAsDataURL(event.target.files[0]);
-            reader.onload = () => {
-                var image = document.getElementById('fotoimg');
-                image.src = reader.result;
-                this.foto = image.src;
-            }
-        },
-    },
-}
+        sendResultResponse('POST', params, url, 'Estudiante registrado correctamente');
+    }
+};
 
 </script>
